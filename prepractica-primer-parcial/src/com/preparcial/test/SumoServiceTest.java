77f3732d0baa267776673ab965eff8264af3fe37
Martin E. Levensohn <martin.levensohn@gmail.com>
2022-04-27 22:10:39 -0300

    Agregados tests

--- prepractica-primer-parcial/src/com/preparcial/test/SumoServiceTest.java
+++ prepractica-primer-parcial/src/com/preparcial/test/SumoServiceTest.java
@@ -1,2 +1,38 @@
-package com.preparcial.test;public class SumoServiceTest {
+package com.preparcial.test;
+
+import com.preparcial.model.Sumo;
+import com.preparcial.service.SumoService;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.matchers.JUnitMatchers.hasItems;
+
+public class SumoServiceTest {
+
+    SumoService sumoService;
+
+    @Test
+    public void obtenerSumosDesdeArchivoTest() {
+        sumoService = new SumoService();
+
+        File file = new File("sumo.in");
+        List<Sumo> obtenido = sumoService.obtenerSumosDesdeArchivo(file);
+
+        Assert.assertThat(obtenido, hasItems(
+                new Sumo(300, 1500),
+                new Sumo(320, 1500),
+                new Sumo(299, 1580),
+                new Sumo(330, 1690),
+                new Sumo(330, 1540),
+                new Sumo(339, 1500),
+                new Sumo(298, 1700),
+                new Sumo(344, 1570),
+                new Sumo(276, 1678),
+                new Sumo(289, 1499)));
+    }
+
 }
--- prepractica-primer-parcial/src/com/preparcial/test/SumoTest.java
+++ prepractica-primer-parcial/src/com/preparcial/test/SumoTest.java
@@ -1,2 +1,41 @@
-package com.preparcial.test;public class SumoTest {
+package com.preparcial.test;
+
+import com.preparcial.model.Sumo;
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class SumoTest {
+
+    Sumo sumo;
+
+    @Test
+    public void dominaATest() {
+        sumo = new Sumo(300, 1500);
+        boolean esperado = true;
+        boolean obtenido = sumo.dominaA(new Sumo(250, 1400));
+        Assert.assertEquals(esperado, obtenido);
+
+        obtenido = sumo.dominaA(new Sumo(250, 1500)) ;
+        Assert.assertEquals(esperado, obtenido);
+
+        obtenido = sumo.dominaA(new Sumo(300, 1400)) ;
+        Assert.assertEquals(esperado, obtenido);
+
+        esperado = false;
+        obtenido = sumo.dominaA(new Sumo(300, 1500)) ;
+        Assert.assertEquals(esperado, obtenido);
+
+        esperado = false;
+        obtenido = sumo.dominaA(new Sumo(400, 1500)) ;
+        Assert.assertEquals(esperado, obtenido);
+
+        esperado = false;
+        obtenido = sumo.dominaA(new Sumo(300, 1600)) ;
+        Assert.assertEquals(esperado, obtenido);
+
+        esperado = false;
+        obtenido = sumo.dominaA(new Sumo(400, 1600)) ;
+        Assert.assertEquals(esperado, obtenido);
+    }
+
 }
